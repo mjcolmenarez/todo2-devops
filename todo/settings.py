0a4e947cf1262f3a517efa1a60980f9a489c1e7e
Maria Jose Colmenarez <mariajosecolmenarez4@gmail.com>
2025-11-30 19:14:51 +0100

    Add WhiteNoise for static files

--- todo/settings.py
+++ todo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 # ---------------------------------------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -94,7 +95,8 @@ USE_TZ = True
 # ---------------------------------------------------------
 # Static files
 # ---------------------------------------------------------
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
